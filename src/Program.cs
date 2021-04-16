c65e8af7ce386c8817d71a35d95dfbf3b9a261c4
Ivan Zlatanov <izlatanov@mavorg.com>
2021-04-16 17:43:06 +0300

    Added the ability to run explicit cases.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Reflection;
 using System.Text;
@@ -12,16 +14,21 @@ namespace WebSocketCompliance
         private const string AgentName = "ClientWebSocket";
         private static readonly Uri BaseUri = new("ws://127.0.0.1:9001");
 
-        static async Task Main()
+        static async Task Main(string[] args)
         {
             object deflateOptions = Activator.CreateInstance(typeof(WebSocket).Assembly.GetType("System.Net.WebSockets.WebSocketDeflateOptions"));
             PropertyInfo deflateOptionsProperty = typeof(ClientWebSocketOptions).GetProperty("DangerousDeflateOptions");
+            int caseCount = 0;
 
-            Console.WriteLine("Fetching case count...");
-            int caseCount = await GetCaseCountAsync();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Fetching case count...");
+                caseCount = await GetCaseCountAsync();
+            }
+
             Memory<byte> buffer = new byte[1024 * 1024];
 
-            for (int caseId = 1; caseId <= caseCount; ++caseId)
+            foreach (int caseId in args.Length > 0 ? ParseCases(args) : Enumerable.Range(1, caseCount))
             {
                 Console.Write($"Running test case {caseId}...");
 
@@ -64,6 +71,31 @@ namespace WebSocketCompliance
             Console.WriteLine("Updating reports...");
             await UpdateReportsAsync();
             Console.WriteLine("Done");
+        }
+
+        private static IEnumerable<int> ParseCases(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                string[] parts = arg.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                int start = int.Parse(parts[0]);
+
+                if (parts.Length == 2)
+                {
+                    int end = int.Parse(parts[1]);
+
+                    while (start <= end)
+                    {
+                        yield return start;
+
+                        start += 1;
+                    }
+                }
+                else
+                {
+                    yield return start;
+                }
+            }
         }
 
         private static async Task<int> GetCaseCountAsync()
